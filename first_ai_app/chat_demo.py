4c248edbcb27eb1280f7f9299335a5ea3c5f8ee1
anmol verma <anmolverma2964@gmail.com>
2026-07-20 21:02:53 +0530

    Add first_ai_app folder and clean repo

---
+++ first_ai_app/chat_demo.py
@@ -0,0 +1,19 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client=genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+
+prompt="explain AI in simple terms to me"
+
+response=client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=prompt
+)
+
+print(response.text)
