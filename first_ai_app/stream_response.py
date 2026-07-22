d566fd5061942c5fa1119b721bc4e87e75375e44
anmol verma <anmolverma2964@gmail.com>
2026-07-22 10:43:15 +0530

    successfully created stream_resposne.py and working prperly

--- first_ai_app/stream_response.py
+++ first_ai_app/stream_response.py
@@ -1,6 +1,7 @@
 from google import genai
 from dotenv import load_dotenv
 import os
+import time
 
 #Load envirnment variable
 #gemini api key from .env file
@@ -16,6 +17,10 @@ client=genai.Client(api_key=API_KEY)
 
 #store history of conversation
 history=[]
+
+#context management
+Max_history = 20
+
 
 system_prompt="You are a helpful assistant and have to follow some rules." \
 "" \
@@ -34,25 +39,37 @@ while True:
         break
     #store user message here
     history.append(f"User: {user_input}")
+
+    if len(history)> Max_history:
+        history=history[-Max_history]
     
     #conversattion context
     conversation_context = system_prompt + "\n"
     conversation_context += "\n".join(history)
+
+    start_time=time.time()
+    full_response=""
     
     try:
         #call gemini model
 
-        response=client.models.generate_content(
+        stream=client.models.generate_content_stream(
             model="gemini-2.5-flash",
             contents=conversation_context
         )
+        for chunk in stream:
+            if chunk.text:
+                print(chunk.text,end="",flush=True)
+                full_response+=chunk.text
         
-        bot_response=response.text
-        print(f"\n Gemini : {bot_response}")
-        
+        end_time=time.time()
+        total_time=end_time-start_time
+        print(f"\nTime taken for response: [{total_time} seconds]")
+       
         # store bot response
-        history.append(f'Assistant: {bot_response}')
+        history.append(f'Assistant: {full_response}')
     except Exception as e:
-        print(f"service is unavailable, please try again later{e}")
+        print(f"error:{e}")
+        continue
 
 
